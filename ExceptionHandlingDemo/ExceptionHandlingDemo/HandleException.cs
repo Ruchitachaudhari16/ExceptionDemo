4e30f0df64c2f21646e58cbb38d33d14a7f937f8
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-07-14 09:41:15 -0400

    [Ruchita]Add:Array Index Exception

---
+++ ExceptionHandlingDemo/ExceptionHandlingDemo/HandleException.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingDemo
+{
+    public  class HandleException
+    {
+
+        public void Display()
+        {
+            try 
+            {
+                //code to try block goes here
+                //put a statement where we want to raise a exception
+
+                int[] myarray = { 10, 20, 30, 40 };
+                for (int val = 0; val < myarray.Length; val++)
+                {
+                    Console.WriteLine(myarray[val]);
+                }
+                Console.WriteLine(myarray[10]);
+            }
+            catch(Exception ex) //Some specficexception. 
+            {
+                //Statement to handle the exception goes here
+                Console.WriteLine(ex.Message);
+            }
+            finally 
+            {
+                //Statement to execute after try and possibly catch block
+                //cleanup code
+            }
+            
+        }
+    }
+}
---
+++ ExceptionHandlingDemo/ExceptionHandlingDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingDemo
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            HandleException handleException = new HandleException();
+            handleException.Display();
+            Console.ReadLine();
+        }
+    }
+}
